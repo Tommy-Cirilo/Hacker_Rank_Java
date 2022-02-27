d979ffa6a5bfce07a383d27e0e983bfac3f43399
Tommy Cirilo-Thorpe <tommy.v.cirilo@gmail.com>
2022-02-27 00:30:17 -0600

    solved problem, checkpoint

---
+++ src/code_wars_js/reversed_words.js
@@ -0,0 +1,14 @@
+//Complete the solution so that it reverses all of the words within the string passed in.
+
+//solution 1
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+  }
+
+
+  //solution2 
+  const reverseWords = (str) => str.split(" ").reverse().join(" ");
+
+
+
+  
