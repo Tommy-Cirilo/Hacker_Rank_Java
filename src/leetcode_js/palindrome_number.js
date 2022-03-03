6420e92ad3cfc8684fd374b45aeab0559cf05688
Tommy Cirilo-Thorpe <tommy.v.cirilo@gmail.com>
2022-03-02 23:40:09 -0600

    completed palindrome number problem

--- src/leetcode_js/palindrome_number.js
+++ src/leetcode_js/palindrome_number.js
@@ -29,3 +29,29 @@ Constraints:
 
 Follow up: Could you solve it without converting the integer to a string? */
 
+var isPalindrome = function(x) {
+    for( var i = 0; i < x.length; i ++) {
+        if (x === x.reverse()) {
+            return true;
+        }
+    }
+};
+
+
+
+//Solution 1
+function isPalindrome(n){  
+    // converts the integer into a string
+    const strNum = String(n);
+    //Condition 1 Edge case
+     if (strNum.length <= 1){
+        return true;
+    }
+    //Condition 2 check if the first substring and the last substring set equal to eachother or not. 
+    if (strNum.substring(0, 1) === strNum.substring(strNum.length - 1, strNum.length)){
+        return isPalindrome(strNum.substring(1, strNum.length -1));
+    }else {
+        return false
+    }
+}
+
